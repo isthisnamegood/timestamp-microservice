32bbb3c87067a17b05165b15ba8632efe58b2e71
Jarvy <jarvy@physiotec.org>
2023-11-09 08:59:01 -0500

    Push to github

---
+++ index.js
@@ -0,0 +1,54 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/:date?", (req, res) => {
+  dateString = req.params.date;
+  let date;
+
+  if (!dateString) {
+    date = new Date();
+  } else {
+    let checkData = dateString / 1;
+    date = isNaN(checkData) ? new Date(dateString) : new Date(checkData);
+  }
+
+  if (date == "Invalid Date") {
+    res.json({error : "Invalid Date"})
+  } else {
+    let unix = date.getTime();
+    let utc = date.toUTCString();
+    res.json({
+      "unix": unix,
+      "utc": utc
+    })
+  }
+})
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(3001, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
